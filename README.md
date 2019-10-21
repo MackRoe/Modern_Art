c325df0be979e98c134f9382e830a1da065ec2b3
MackRoe <elaine.music@students.makeschool.com>
2019-10-20 17:08:14 -0700

    edit README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+created from tutorials at codeclubprojects.org
+
+# turtles.py
+creates turtle art
--- turtles.py
+++ turtles.py
@@ -5,24 +5,29 @@ from random import *
 def random_color():
     red = randint(0, 255)
     green = randint(0, 255)
-    blue = randint(0,255)
+    blue = randint(0, 255)
     color(red, green, blue)
 
 
 def random_place():
     penup()
-    x = randint(-100,100)
-    y = randint(-100,100)
-    goto(x,y)
+    x = randint(-100, 100)
+    y = randint(-100, 100)
+    goto(x, y)
     pendown()
 
-shape('turtle')
-random_color()
-random_place()
-stamp()
-random_color()
-random_place()
-stamp()
 
-  if __name__ == '__main__':
+def random_heading():
+    setheading(randint(0, 360))
+
+
+shape('turtle')
+for i in range(30):
+    random_color()
+    random_heading()
+    random_place()
+    stamp()
+
+
+if __name__ == '__main__':
     app.run(debug=True)
